17ffa781e5d299d46103ee56622f006e80d53d82
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-15 11:55:57 -0500

    Update Intern.test.js

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,16 +1,36 @@
-const Intern = require('../lib/Intern')
+const Intern = require('../lib/Intern');
 
-describe('Intern', () => {
-    
-    //passes
-    it("getSchool returns school", () => {
-        expect(new Intern("Northwestern University").getName()).toBe("Northwestern University");
+describe('Intern class', () => {
+    // Test if class is created as expected
+    it('Creates new intern with correct values', () => {
+        const internName = 'intern';
+        const internID = 3;
+        const internEmail = 'intern@team.com';
+        const internSchool = 'school'
+        const intern = new Intern(internName, internID, internEmail, internSchool);
+        expect(intern.name).toEqual('intern');
+        expect(intern.id).toEqual(3);
+        expect(intern.email).toEqual('intern@team.com');
+        expect(intern.school).toEqual('school');
     });
-
-    //passes
-    it("getRole returns role", () => {
-        expect(new Intern("Employee").getRole()).toBe("Intern");
-    });
-
-
-})
+    // Test if getSchool returns correct school
+    describe("Intern", () => {
+        const intern = new Intern();
+        const getSchoolSpy = jest.spyOn(intern, "getSchool")
+        describe(".getSchool", () => {
+            test("Returns correct school with getSchool()", () => {
+                expect(intern.getSchool()).toBe(intern.school)
+            })
+        })
+    })
+    // Test if getRole returns correct role
+    describe("Intern", () => {
+        const intern = new Intern();
+        const getRoleSpy = jest.spyOn(intern, "getRole")
+        describe(".getRole", () => {
+            test("Returns correct role with getRole()", () => {
+                expect(intern.getRole()).toBe('Intern')
+            })
+        })
+    })
+});
