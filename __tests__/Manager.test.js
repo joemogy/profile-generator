f8d2f38dd1fb7e4b95a169bfb9c8842d5bd9977a
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-15 11:55:22 -0500

    Update Manager.test.js

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,16 +1,35 @@
-const Manager = require('../lib/Manager')
+const Manager = require('../lib/Manager');
 
-describe('Intern', () => {
-    
-    //passes
-    it("getOfficeNumber returns officeNumber", () => {
-        expect(new Manager("123").getName()).toBe("123");
+describe('manager class', () => {
+    it('Creates new manager with correct values', () => {
+        const managerName = 'manager';
+        const managerID = 1;
+        const managerEmail = 'manager@team.com';
+        const officeNumber = 1;
+        const manager = new Manager(managerName, managerID, managerEmail, officeNumber);
+        expect(manager.name).toEqual('manager');
+        expect(manager.id).toEqual(1);
+        expect(manager.email).toEqual('manager@team.com');
+        expect(manager.officeNumber).toEqual(1);
     });
-
-    //passes
-    it("getRole returns role", () => {
-        expect(new Manager("Employee").getRole()).toBe("Manager");
-    });
-
-
-})
+    // Test if getOfficeNumber returns correct school
+    describe("Manager", () => {
+        const manager = new Manager();
+        const getOfficeNumberSpy = jest.spyOn(manager, "getOfficeNumber")
+        describe(".getOfficeNumber", () => {
+            test("Returns correct school with getOfficeNumber()", () => {
+                expect(manager.getOfficeNumber()).toBe(manager.officeNumber)
+            })
+        })
+    })
+    // Test if getRole returns correct role
+    describe("Manager", () => {
+        const manager = new Manager();
+        const getRoleSpy = jest.spyOn(manager, "getRole")
+        describe(".getRole", () => {
+            test("Returns correct role with getRole()", () => {
+                expect(manager.getRole()).toBe('Manager')
+            })
+        })
+    })
+});
