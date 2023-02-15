80e4d63f98a7d61114877c861206d5c08b5e1e02
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-15 11:51:20 -0500

    Update generateHTML.js

--- dist/generateHTML.js
+++ dist/generateHTML.js
@@ -1,1 +1,26 @@
+const htmlPrefix =
+`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${teamName}</title>
+</head>
+<body>
+    <header>
+        ${teamName}
+    </header>
+    <main>
+        <!-- Insert programatically generated html here -->
+`
+const htmlSufix =
+`
+    </main>
+</body>
+</html>
+`;
 
+
+const generateHTML = () => {}
