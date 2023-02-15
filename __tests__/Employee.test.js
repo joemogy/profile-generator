e179e3fc61c72809d16f4d59e1612cfa2306d35e
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-15 11:56:51 -0500

    Update Employee.test.js

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,26 +1,63 @@
-const Employee = require('../lib/Employee')
-
-describe('Employee', () => {
-    
-    //passes
-    it("getName returns name", () => {
-        expect(new Employee("Charles").getName()).toBe("Charles");
+const Employee = require('../lib/Employee');
+describe('Employee class', () => {
+    // Test if class is created as expected
+    it('Creates new employee with correct values', () => {
+        const employeeName = 'Employee';
+        const employeeID = 1;
+        const employeeEmail = 'employee@team.com';
+        const employee = new Employee(employeeName, employeeID, employeeEmail);
+        expect(employee.name).toEqual('Employee');
+        expect(employee.id).toEqual(1);
+        expect(employee.email).toEqual('employee@team.com');
     });
-
-    //passes
-    it("getId returns id", () => {
-        expect(new Employee("Charles", "123").getId()).toBe("123");
-    });
-
-    //passes
-    it("getEmail returns email address", () => {
-        expect(new Employee("Charles", "123", "cdfishe1@mail.com").getEmail()).toBe("cdfishe1@mail.com");
-    });
-
-    //passes
-    it("getRole returns role", () => {
-        expect(new Employee("Employee").getRole()).toBe("Employee");
-    });
-
-
-})
+    // Test if getName returns correct role
+    describe("Employee", () => {
+        const employeeName = 'Employee';
+        const employeeID = 1;
+        const employeeEmail = 'employee@team.com';
+        const employee = new Employee(employeeName, employeeID, employeeEmail);
+        const getNameSpy = jest.spyOn(employee, "getName")
+        describe(".getName", () => {
+            test("Returns correct name with getName()", () => {
+                expect(employee.getName()).toBe(employee.name)
+            })
+        })
+    })
+    // Test if getId returns correct role
+    describe("Employee", () => {
+        const employeeName = 'Employee';
+        const employeeID = 1;
+        const employeeEmail = 'employee@team.com';
+        const employee = new Employee(employeeName, employeeID, employeeEmail);
+        const getIdSpy = jest.spyOn(employee, "getId")
+        describe(".getId", () => {
+            test("Returns correct ID with getId()", () => {
+                expect(employee.getId()).toBe(employee.id)
+            })
+        })
+    })
+    // Test if getEmail returns correct role
+    describe("Employee", () => {
+        const employeeName = 'Employee';
+        const employeeID = 1;
+        const employeeEmail = 'employee@team.com';
+        const employee = new Employee(employeeName, employeeID, employeeEmail);
+        const getEmailSpy = jest.spyOn(employee, "getEmail")
+        describe(".getEmail", () => {
+            test("Returns correct email with getEmail()", () => {
+                expect(employee.getEmail()).toBe(employee.email)
+            })
+        })
+    })
+    // Test if getRole returns correct role
+    describe("Employee", () => {
+        const employee = new Employee();
+        const getRoleSpy = jest.spyOn(employee, "getRole")
+        describe(".getRole", () => {
+            test("Returns correct role with getRole()", () => {
+                expect(employee.getRole()).toBe('Employee')
+            })
+        })
+    })
+});
+  
