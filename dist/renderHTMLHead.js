a27f041c131a434a5b2cf9c52cfdff65f2ace305
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-15 11:52:25 -0500

    Update renderHTMLHead.js

--- dist/renderHTMLHead.js
+++ dist/renderHTMLHead.js
@@ -1,1 +1,22 @@
+const renderHTMLHead = () => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Summary</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="./src/style.css">
+    </head>
+    <body>
+        <header>
+            <h1>Team Summary</h1>
+        </header>
+        <main>`;
+}
 
+module.exports = renderHTMLHead;
