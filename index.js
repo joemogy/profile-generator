13e45a7a7ea7b71b4cb27313d47210feb1f13e97
Joseph Mogavero <82268976+joemogy@users.noreply.github.com>
2023-02-15 11:44:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,209 +1,136 @@
-// Modules needed for the script
+// Import Classes
+const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+// Import packages
 const inquirer = require('inquirer');
-const createHtml = require('./dist/createHtml');
 const fs = require('fs');
+const { data } = require('browserslist');
+const concatenateHTMLArray = require('./dist/concatenateHTMLArray');
+const renderHTMLHead = require('./dist/renderHTMLHead');
+const renderHTMLTail = require('./dist/renderHTMLTail');
 
-//Empty array where responses of prompt data will be pushed
-let team = [];
+// const initialPrompt = () => {
+//     return inquirer.prompt([
+//         {
+//             type: 'input',
+//             message: "What is your team\'s name?",
+//             name: 'name',
+//         },
+//     ]).then((data) => {
+//         fs.writeFileSync(`team.html`,concatenateHTMLArray(data));
+//         managerPromt();
+//     })
+// };
 
-//A function that will validate prompts needing a string
-const validateString = string => {
-    return string !== '' || 'This information is required.';
-}
-
-//A function that will validate if a number is entered in prompts needing a number
-const validateNumber = number => {
-    const reg = /^\d+$/;
-    return reg.test(number) || "Please enter a number.";
-}
-
-//A function that will validate if an proper email is entered when prompted.
-const validateEmail = email => {
-    const reg = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/;
-    return reg.test(email) || "Please enter a valid email."
-
-}
-
-
-//Begins prompt process with asking for manager data
-const init = () => {
-    inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'name',
-        message: "What is the manager's name?",
-        validate: validateString,
-      },
-      {
-        type: 'input',
-        message: "What is the manager's ID number?",
-        name: 'id',
-        validate: validateNumber,
-        
-      },
-      {
-        type: 'input',
-        message: "What is the manager's email address?",
-        name: 'email',
-        validate: validateEmail,
-      
-      },
-  
-      {
-          type: 'input',
-          message: "What is the manager's office number?",
-          name: 'office',
-          validate: validateNumber,
-        
+const managerPromt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the team manager\'s name?",
+            name: 'name',
         },
-    ])
-    .then((data) => {
-      let manager = new Manager(data.name, data.id, data.email, data.office);
-      team.push(manager);
-      addTeamMember();
-      
-  });
-}
-
-
-//Asks to create another team member
-const addTeamMember = () => {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                message: 'What employee would you like to add?',
-                name: 'employee',
-                choices: ['Engineer', 'Intern', "I don't wish to add another team member"],
-            },
-        ])
-        .then((data) => {
-            if(data.employee === 'Engineer') {
-                createEngineer();
-            } else if (data.employee === 'Intern') {
-                createIntern();
-            } else {
-                renderTeam();
-            }
-        });
+        {
+            type: 'input',
+            message: "What is the team manager's ID",
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: "What is the team manager's email?",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "What is the team manager's office number?",
+            name: 'officeNumber',
+        },
+    ]).then((data) => {
+        fs.writeFileSync(`team.html`,renderHTMLHead());
+        fs.appendFileSync(`team.html`,concatenateHTMLArray(data));
+        teamMemberPromt();
+    })
 };
 
-//Prompts to create engineer
-const createEngineer = () => {
-    inquirer
-        .prompt([
-            {
-            type: 'input',
-            name: 'name',
-            message: "What is the engineer's name?",
-            validate: validateString,
-            },
-            {
-            type: 'input',
-            message: "What is the engineer's ID number?",
-            name: 'id',
-            validate: validateNumber,
-            
-            },
-            {
-            type: 'input',
-            message: "What is the engineer's email address?",
-            name: 'email',
-            validate: validateEmail,
-            
-            },
-
-            {
-                type: 'input',
-                message: "What is the engineer's GitHub id?",
-                name: 'github',
-                validate: validateString,
-            
-            },
-        ])
-        .then((data) => {
-            let engineer = new Engineer(data.name, data.id, data.email, data.github);
-            team.push(engineer);
-            moreTeamMembers();
-            
-        });
-}
-
-//Prompts to create intern
-const createIntern = () => {
-    inquirer
-        .prompt([
-            {
-            type: 'input',
-            name: 'name',
-            message: "What is the intern's name?",
-            validate: validateString,
-            },
-
-            {
-            type: 'input',
-            message: "What is the intern's ID number?",
-            name: 'id',
-            validate: validateNumber,
-            
-            },
-            {
-            type: 'input',
-            message: "What is the intern's email address?",
-            name: 'email',
-            validate: validateEmail,
-            
-            },
-
-            {
-                type: 'input',
-                message: "What is the intern's school?",
-                name: 'school',
-                validate: validateString,
-            
-            },
-        ])
-        .then((data) => {
-            let intern = new Intern(data.name, data.id, data.email, data.school);
-            team.push(intern);
-            moreTeamMembers();
-            
-        });
-}
-
-//Asks if addtional team members should be added
-const moreTeamMembers = () => {
-    inquirer
-        .prompt([
-            {
+const teamMemberPromt = () => {
+    return inquirer.prompt([
+        {
             type: 'list',
-            name: 'more',
-            message: "Do you wish to add another team member?",
-            choices: ['Yes', 'No'],
-            }
-            
-        ])
-        .then((data) => {
-            if(data.more === 'Yes') {
-                addTeamMember();
-            } else {
-                renderTeam();
-            }
-            
-        });
-}
+            message: "Which type of team member would you like to add?",
+            name: 'teamMember',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add anymore team members']
+        },
+    ]).then((data) => {
+        if(data.teamMember === 'Engineer') {
+            engineerPrompt();
+        } else if (data.teamMember === 'Intern') {
+            internPrompt();
+        } else {
+            fs.appendFileSync(`team.html`,renderHTMLTail());
+            console.log('team.html file successfully generated!')
+        }
+    })
+};
 
-//Takes all classes pushed into the team array, and passes that into the createCard function to write the html file
-const renderTeam = () => {
-  let mockHtml =  createHtml(team);
-  fs.writeFileSync('index.html', mockHtml, function(err) {
+const engineerPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the engineer\'s name?",
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's ID",
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's email?",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "What is the engineer's github username?",
+            name: 'github',
+        },
+    ]).then((data) => {
+        fs.appendFileSync(`team.html`,concatenateHTMLArray(data));
+        teamMemberPromt();
+    })
+};
 
-  })
- };
+const internPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the intern\'s name?",
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: "What is the intern's ID",
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: "What is the intern's email?",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "What school did the intern attend?",
+            name: 'school',
+        },
+    ]).then((data) => {
+        fs.appendFileSync(`team.html`,concatenateHTMLArray(data));
+        teamMemberPromt();
+    })
+};
 
- //Begins execution of the prompt process
+const init = () => {
+    managerPromt();
+};
+
 init();
